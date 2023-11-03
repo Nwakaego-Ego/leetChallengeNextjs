cbe075870d8333c8ec7e11dff8f6e7134e94324c
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-11-03 15:58:24 -0700

    count

--- app/Counter/Counter.js
+++ app/Counter/Counter.js
@@ -1,52 +1,73 @@
-"use client";
+// "use client";
+// import React, { useState } from "react";
+
+// import "./Counter.css";
+
+// const Counter = () => {
+//   const [increment, setIncrement] = useState(0);
+
+//   const handleClick = () => {
+//     setIncrement(increment + 1);
+//   };
+
+//   return (
+//     <>
+//       <div className="">
+//         <div>
+//           <button
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded p-10"
+//             onClick={handleClick}
+//           >
+//             Increment
+//           </button>
+//           <p>increment : {increment}</p>
+//         </div>
+//         <div className="">
+//           <div className="container">
+//             <div className="">
+//               <div className="header">
+//                 <img src="./master-logo.png" className="logo" />
+//                 <h4 className="masterCard">Kaego's Card</h4>
+//                 <img src="./master-chip.png" className="chip" />
+//               </div>
+//               <div>
+//                 <div className="card-valid">
+//                   <div>Card Number</div>
+//                   <div className="valid vd">Valid thru</div>
+//                 </div>
+//                 <div className="code">
+//                   <div className="number">6024 5893 2438 4801 </div>
+//                   <div className="date vd">04/17</div>
+//                 </div>
+//                 <p className="nwakaego">Nwakaego Nwaekpe</p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Counter;
+
 import React, { useState } from "react";
 
-import "./Counter.css";
+function example() {
+  const [count, setCount] = useState(0);
 
-const Counter = () => {
-  const [increment, setIncrement] = useState(0);
-
-  const handleClick = () => {
-    setIncrement(increment + 1);
-  };
+  function handleClick() {
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+  }
 
   return (
     <>
-      <div className="">
-        <div>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded p-10"
-            onClick={handleClick}
-          >
-            Increment
-          </button>
-          <p>increment : {increment}</p>
-        </div>
-        <div className="">
-          <div className="container">
-            <div className="">
-              <div className="header">
-                <img src="./master-logo.png" className="logo" />
-                <h4 className="masterCard">Kaego's Card</h4>
-                <img src="./master-chip.png" className="chip" />
-              </div>
-              <div>
-                <div className="card-valid">
-                  <div>Card Number</div>
-                  <div className="valid vd">Valid thru</div>
-                </div>
-                <div className="code">
-                  <div className="number">6024 5893 2438 4801 </div>
-                  <div className="date vd">04/17</div>
-                </div>
-                <p className="nwakaego">Nwakaego Nwaekpe</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div>Count</div>
+      <button onClick={handleClick}>Count</button>
     </>
   );
-};
+}
 
-export default Counter;
+export default example;
--- app/Qrcode/qrcode.jsx
+++ app/Qrcode/qrcode.jsx
@@ -1,48 +1,19 @@
-// "use client";
-// import React, { useState } from "react";
-// import QRCode from "qrcode";
-// import "./qrcode.css";
-
-// const qrcode = () => {
-//   const [qr, setQr] = useState("");
-
-//   const generateQr = () => {
-//     setQr(
-//       "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example"
-//     );
-//   };
-//   return (
-//     <>
-//       <div>
-//         <div className="text-white">QRCODE GENERATOR</div>
-//         <input type="text" />
-//         <button onClick={generateQr}>Generate Code</button>
-//         <p>{qr}</p>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default qrcode;
-
 "use client";
 
 import React, { useState } from "react";
-import QRCode from "qrcode.react"; // Import the QRCode component
-import "./qrcode.css";
+import QRCode from "qrcode.react";
 
 const QrCode = () => {
-  const [qrData, setQrData] = useState(""); // Use a state variable for the QR code data
+  const [qrData, setQrData] = useState("");
 
   const generateQr = () => {
-    // Retrieve the data from the input field and update the state
     const inputData = document.getElementById("qr-input").value;
     setQrData(inputData);
   };
 
   return (
     <>
-      <div>
+      <div className="bg-red-600 w-full h-full ">
         <div className="text-white">QR CODE GENERATOR</div>
         <input
           type="text"
@@ -51,7 +22,7 @@ const QrCode = () => {
           className="text-blue-600"
         />
         <button onClick={generateQr}>Generate Code</button>
-        {/* Render the QRCode component with the updated data */}
+
         <QRCode value={qrData} />
       </div>
     </>
