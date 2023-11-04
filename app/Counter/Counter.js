7ce2b5398983c4b5b160b6eafe5c072ea19732fc
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-11-04 09:51:43 -0700

    form function

--- app/Counter/Counter.js
+++ app/Counter/Counter.js
@@ -1,52 +1,81 @@
-"use client";
-import React, { useState } from "react";
+// "use client";
+// import React, { useState } from "react";
 
-import "./Counter.css";
+// import "./Counter.css";
 
-const Counter = () => {
-  const [increment, setIncrement] = useState(0);
+// const Counter = () => {
+//   const [increment, setIncrement] = useState(0);
 
-  const handleClick = () => {
-    setIncrement(increment + 1);
-  };
+//   const handleClick = () => {
+//     setIncrement(increment + 1);
+//   };
+
+//   return (
+//     <>
+//       <div className="">
+//         <div>
+//           <button
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded p-10"
+//             onClick={handleClick}
+//           >
+//             Increment
+//           </button>
+//           <p>increment : {increment}</p>
+//         </div>
+//         <div className="">
+//           <div className="container">
+//             <div className="">
+//               <div className="header">
+//                 <img src="./master-logo.png" className="logo" />
+//                 <h4 className="masterCard">Kaego's Card</h4>
+//                 <img src="./master-chip.png" className="chip" />
+//               </div>
+//               <div>
+//                 <div className="card-valid">
+//                   <div>Card Number</div>
+//                   <div className="valid vd">Valid thru</div>
+//                 </div>
+//                 <div className="code">
+//                   <div className="number">6024 5893 2438 4801 </div>
+//                   <div className="date vd">04/17</div>
+//                 </div>
+//                 <p className="nwakaego">Nwakaego Nwaekpe</p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Counter;
+
+import React from "react";
+import { useFormStatus } from "react-dom";
+import { submitForm } from "./actions.js";
+
+// Component for the submit button
+function Submit() {
+  const { pending } = useFormStatus();
 
   return (
-    <>
-      <div className="">
-        <div>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded p-10"
-            onClick={handleClick}
-          >
-            Increment
-          </button>
-          <p>increment : {increment}</p>
-        </div>
-        <div className="">
-          <div className="container">
-            <div className="">
-              <div className="header">
-                <img src="./master-logo.png" className="logo" />
-                <h4 className="masterCard">Kaego's Card</h4>
-                <img src="./master-chip.png" className="chip" />
-              </div>
-              <div>
-                <div className="card-valid">
-                  <div>Card Number</div>
-                  <div className="valid vd">Valid thru</div>
-                </div>
-                <div className="code">
-                  <div className="number">6024 5893 2438 4801 </div>
-                  <div className="date vd">04/17</div>
-                </div>
-                <p className="nwakaego">Nwakaego Nwaekpe</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <button type="submit" disabled={pending}>
+      {pending ? "Submitting..." : "Submit"}
+    </button>
   );
-};
+}
 
-export default Counter;
+// Component for the form
+function Form({ action }) {
+  return (
+    <form action={action}>
+      <Submit />
+    </form>
+  );
+}
+
+// Main App component
+export default function App() {
+  return <Form action={submitForm} />;
+}
